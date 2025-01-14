e5092bdd3ad472fd036a8b0c9d9200fb1d16e9fc
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2025-01-13 21:36:42 -0500

    Update quantum_bridge_wrapper.py

--- src/quantum_bridge_wrapper.py
+++ src/quantum_bridge_wrapper.py
@@ -1,4 +1,8 @@
 class QuantumBridgeWrapper:
+    """
+    Wrapper for interacting with quantum entanglement backends.
+    """
+
     def __init__(self, backend="quantum_lib"):
         self.backend = backend.lower()
         if self.backend == "quantum_lib":
@@ -8,7 +12,18 @@ class QuantumBridgeWrapper:
             import qiskit
             self.lib = qiskit
         else:
-            raise ValueError("Unsupported quantum backend. Choose 'quantum_lib' or 'qiskit'.")
+            raise ValueError(
+                "Unsupported quantum backend. Choose 'quantum_lib' or 'qiskit'."
+            )
 
     def create_entanglement(self, *args, **kwargs):
+        """
+        Create an entanglement using the chosen backend.
+        """
         return self.lib.create_entanglement(*args, **kwargs)
+
+    def validate_entanglement(self, entanglement_id: str) -> bool:
+        """
+        Validate an entanglement using the chosen backend.
+        """
+        return self.lib.validate_entanglement(entanglement_id)
