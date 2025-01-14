6fa4ece0a97df8f1554860382e28bbaf58dfe338
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2025-01-13 20:10:31 -0500

    Update test_quantum_resource_manager.py

--- tests/test_quantum_resource_manager.py
+++ tests/test_quantum_resource_manager.py
@@ -1,11 +1,13 @@
+import unittest
 from src.quantum_resource_manager import QuantumResourceManager
+
 
 class TestQuantumResourceManager(unittest.TestCase):
     def setUp(self):
         self.manager = QuantumResourceManager()
 
     def test_resource_allocation_and_release(self):
-        allocation = self.manager.allocate_resource("task1", 5)
+        self.manager.allocate_resource("task1", 5)
         self.assertIn("task1", self.manager.resources)
         self.assertEqual(self.manager.resources["task1"], 5)
 
