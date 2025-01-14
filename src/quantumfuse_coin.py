9edc974e453b6719af78e538d385727c10b1ab11
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2025-01-14 15:08:39 -0500

    Update quantumfuse_coin.py

--- src/quantumfuse_coin.py
+++ src/quantumfuse_coin.py
@@ -5,6 +5,7 @@ import logging
 logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s: %(message)s")
 logger = logging.getLogger(__name__)
 
+
 class QuantumFuseCoin:
     """Manage QFC supply and distribution."""
 
