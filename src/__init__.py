6ab89f897e719232ee0395bff127ce01465c7ace
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2025-01-15 10:32:30 -0500

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,11 @@
 # Import core functionality
 from .core import Blockchain, StateManager, Transaction
 
-# Import services
+# Import services and management
 from .services import QuantumServices
+from .consensus_and_governance import ConsensusManager
+from .quantum_secure_manager import QuantumSecureManager
+from .quantum_resource_manager import QuantumResourceManager
 
 # Import contract-related functionality
 from .quantum_smart_contract import QuantumSmartContract
@@ -13,14 +16,20 @@ from .quantum_ai_integration_contract import QuantumAIIntegrationContract
 from .quantum_random_oracle_contract import QuantumRandomOracleContract
 from .quantum_defi_contract import QuantumDefiContract
 
-# Import infrastructure
-from .quantum_node import QuantumNode, PeerManager,
-TransactionPropagator, ConsensusManager
-
-# Import utilities
-from .quantum_wallet import QuantumWallet
+# Import quantum infrastructure and environment
+from .quantum_node import QuantumNode, PeerManager, TransactionPropagator
 from .quantum_bridge import QuantumBridge
-from .quantum_interface import QuantumInterface, get_quantum_adapter  # Fixed import
+from .quantum_interface import QuantumInterface, get_quantum_adapter
+from .fusion_reactor import FusionReactor
+from .ar_vr_xr_environment import ARVREnvironment
+
+# Import utilities and wrappers
+from .quantum_wallet import QuantumWallet
+from .quantum_simulator import QuantumSimulator
+from .quantumfuse_coin import QuantumFuseCoin
+from .backend_selector import BackendSelector
+from .pqc_wrapper import PQCWrapper
+from .quantum_bridge_wrapper import QuantumBridgeWrapper
 
 # Expose key components for simpler imports in external modules
 __all__ = [
@@ -28,6 +37,9 @@ __all__ = [
     "StateManager",
     "Transaction",
     "QuantumServices",
+    "ConsensusManager",
+    "QuantumSecureManager",
+    "QuantumResourceManager",
     "QuantumSmartContract",
     "QuantumCommunicationContract",
     "QuantumGovernanceContract",
@@ -38,9 +50,15 @@ __all__ = [
     "QuantumNode",
     "PeerManager",
     "TransactionPropagator",
-    "ConsensusManager",
     "QuantumWallet",
     "QuantumBridge",
     "QuantumInterface",
-    "get_quantum_adapter"  # No trailing comma here
+    "get_quantum_adapter",
+    "FusionReactor",
+    "ARVREnvironment",
+    "QuantumSimulator",
+    "QuantumFuseCoin",
+    "BackendSelector",
+    "PQCWrapper",
+    "QuantumBridgeWrapper"
 ]
