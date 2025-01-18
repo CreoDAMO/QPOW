6202ccb398288ebbb2c44206b88dcf1b5f0c36e7
Jacque Antoine DeGraff <jacquedegraff@creodamo.com>
2025-01-18 16:39:45 -0500

    Update src

---
+++ src/src/qdpos_manager.py
@@ -0,0 +1,19 @@
+class QDPoSManager:
+    """Manages Quantum Delegated Proof-of-Stake 
+(QDPoS) consensus operations."""
+
+
+    def __init__(self):
+        self.delegates = {}
+
+
+			def register_delegate(self, holder_address: str, validator_address: str):
+        """Register a delegate supporting a validator."""
+        self.delegates[holder_address] = validator_address
+
+
+    def select_validator(self):
+        """Select a validator based on delegated support (stub)."""
+        if self.delegates:
+            return next(iter(self.delegates.values()))  # Simplified selection
+        return None
